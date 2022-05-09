b0d2395ec6a9a79824bb0c8dba419ed753aa43b6
Caio Machado <caio@jgwebcom.com>
2022-05-09 13:35:18 -0300

    Server finished

---
+++ README.md
@@ -0,0 +1,13 @@
+# SOLID
+
+1. Single Responsibility Principle
+2. Open/Closed Principle
+3. Liskov Substitution Principle
+4. Interface Segregation Principle
+5. Dependency Inversion Principle
+
+6. Cada classe/função tem uma responsabilidade única
+7. As classes da aplicação devem ser abertas para extensão mas fechadas para modificação
+8. Nós devemos poder substituir uma classe pai por uma herança dela e tudo continuar funcionando
+9. ...
+10. ...
---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "07e76c97cd3abb",
+    pass: "72bc292846b54a"
+  }
+});
+
+export class NodeMailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Caio Machado <caiomachado1102@gmail.com>',
+      subject,
+      html: body,
+    })
+  };
+}
